6026b123b7b8e93a14f66e82c184738cb7105345
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-06-06 22:51:09 +0530

    bs4

--- main.py
+++ main.py
@@ -9,4 +9,11 @@ with open("website.html") as file:
 soup = BeautifulSoup(contents, "html.parser")
 #print(soup.title.string)
 #print(soup.prettify())
-print(soup.a)
+#print(soup.a)
+#print(soup.p)
+
+all_anchour_tags = soup.find_all(name="a")
+#print(all_anchour_tags)
+
+for tag in all_anchour_tags:
+    print(tag.getText())
