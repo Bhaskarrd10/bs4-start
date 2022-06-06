6e64466f0a2788bc422671ed51c9e38b512ef22c
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-06-06 22:46:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from bs4 import BeautifulSoup
+#import lxml
+
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+#print(soup.title.string)
+#print(soup.prettify())
+print(soup.a)
